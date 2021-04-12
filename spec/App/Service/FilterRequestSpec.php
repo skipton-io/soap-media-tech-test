eb3a5ddbdd29886549f2f1f00307118093a836e0
Mark Smith <mark.smith@9xb.com>
2021-04-12 22:04:21 +0100

    Added test that is failing

---
+++ spec/App/Service/FilterRequestSpec.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace spec\App\Service;
+
+use App\Service\FilterRequest;
+use PhpSpec\ObjectBehavior;
+
+class FilterRequestSpec extends ObjectBehavior
+{
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(FilterRequest::class);
+    }
+
+    function it_matches_int()
+    {
+        $this::toInt('123')->shouldBe(123);
+        $this::toInt('123.45')->shouldBe(null);
+        $this::toInt('a123')->shouldBe(null);
+    }
+
+    function it_matches_array()
+    {
+        $this::inArray('test', ['test'])->shouldBe('test');
+        $this::inArray('difficulty', ['user', 'score', 'difficulty'])->shouldBe('difficulty');
+    }
+}
