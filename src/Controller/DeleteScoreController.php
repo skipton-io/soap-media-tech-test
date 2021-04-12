6c0fa6635660d061aea3b1d5aa75c95dc3ea2e26
Mark Smith <mark.smith@9xb.com>
2021-04-12 22:52:14 +0100

    Added ability to delete score (if admin)

--- src/Controller/DeleteScoreController.php
+++ src/Controller/DeleteScoreController.php
@@ -2,20 +2,50 @@
 
 namespace App\Controller;
 
+use App\Repository\ScoresRepositoryInterface;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\RouterInterface;
 
 class DeleteScoreController extends AbstractController
 {
-    /**
-     * @Route("/admin/delete/score", name="delete_score")
-     */
-    public function index(): Response
+    protected $scoresRepository;
+
+    protected $entityManager;
+
+    public function __construct(ScoresRepositoryInterface $scoresRepository, EntityManagerInterface $entityManager)
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/DeleteScoreController.php',
+        $this->scoresRepository = $scoresRepository;
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @Route("/admin/delete/score/{id}", name="delete_score", methods={"GET", "POST"}, requirements={"id"="\d+"})
+     */
+    public function index(int $id, Request $request, RouterInterface $router): Response
+    {
+        if ($request->isMethod('POST')) {
+            $submittedToken = $request->request->get('token');
+            if ($this->isCsrfTokenValid('delete-score', $submittedToken)) {
+                $entity = $this->scoresRepository->find($id);
+                if ($entity) {
+                    $this->entityManager->remove($entity);
+                    $this->entityManager->flush();
+
+                    $referrer = $request->request->get('_referrer');
+                    if (!$referrer) {
+                        $referrer = $router->generate('homepage');
+                    }
+
+                    return $this->redirect($referrer);
+                }
+            }
+        }
+        return $this->render('delete-score/index.html.twig', [
+            'referrer'=> $request->query->get('_referrer')
         ]);
     }
 }
